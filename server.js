eca602f7b4aded92bb99eeb9ca31f78a2f804d22
Matthew Villwock <mtvillwock@gmail.com>
2015-03-04 22:37:06 -0800

    practice server v2

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var http = require("http");
 
-function onRequest(request, response) {
-  // Where are request and response coming from?
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}
-// Where are request and response coming from?
-http.createServer(onRequest).listen(8888);
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
 
-console.log("Server has started.");
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
