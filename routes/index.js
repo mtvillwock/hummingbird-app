54d5513f7741429ab4352d5d43ed0847f2531e79
Matthew Villwock <mtvillwock@gmail.com>
2015-03-06 15:36:39 -0800

    added new message routes, but it's not working... not sure why

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 //all routes go below (Inside of module.exports block)
 module.exports = function (app) {
   app.get('/', function(req,res){
-    res.render("index")
+    res.render("index");
+  });
+
+  app.get('/new_message', function(req, res){
+    res.render("new_message");
+  });
+
+  app.get('*', function(req, res){
+    notFound(res);
   });
 };
 
+function notFound(res) {
+  res.send('<h1>Page not found.</h1>', 404);
+}
