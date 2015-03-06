9ee96ab871e466491500dfb72d9ac4b07069245a
Matthew Villwock <mtvillwock@gmail.com>
2015-03-05 20:42:29 -0800

    add fs dependancy and use to read uploaded info

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,5 @@
 var querystring = require("querystring");
-
-var exec = require("child_process").exec;
+    fs = require("fs");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -23,14 +22,20 @@ var body = '<html>'+
     response.end();
 }
 
-
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + postData);
+  response.write("You've sent the text: " + postData);
   querystring.parse(postData).text);
   response.end();
 }
 
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
