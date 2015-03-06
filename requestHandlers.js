3b1cc5b16e9fd60bf7629abe285e5581ac4cc600
Matthew Villwock <mtvillwock@gmail.com>
2015-03-05 21:04:34 -0800

    punting on error and moving to handle post upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -17,7 +17,7 @@ var body = '<html>'+
  '</body>'+
  '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
 }
@@ -25,8 +25,7 @@ var body = '<html>'+
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: " + postData);
-  querystring.parse(postData).text);
+  response.write("You've sent the text: " + querystring.parse(postData).text);
   response.end();
 }
 
